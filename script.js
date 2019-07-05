c1e98f8cb09e820a00d6ada0d2ee14bbba8d7712
Fnaz40 <48803891+Fnaz40@users.noreply.github.com>
2019-07-06 02:46:14 +0500

    responsive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-var val = 0, purchase = 0, count_ap = 0, count_ban = 0, count_org = 0, total_purchase = 0;
+let val = 0, purchase = 0, count_ap = 0, count_ban = 0, count_org = 0, total_purchase = 0;
 function values() {
     val = Number(document.querySelector("input").value);
 }
@@ -33,33 +33,45 @@ function increaseValue() {
 }
 function increaseValueTwo() {
     values();
-    if (val >= 15) {
-        count_org += 1;
-        purchase += 15;
-        val = val - purchase;
-        total_purchase = purchase + total_purchase;
-        purchase = 0;
-        document.querySelector("#number-two").value = count_org; console.log(val);
-        result();
+    // if (val > total_purchase) {
+        if (val >= 15) {
+            count_org += 1;
+            purchase += 15;
+            val = val - purchase;
+            total_purchase = purchase + total_purchase;
+            purchase = 0;
+            document.querySelector("#number-two").value = count_org;
+            result();
 
+        }
+        else {
+            alert("Not Enough Money to buy Orange")
+        }
     }
-    else {
-        alert("Not Enough Money to buy Orange")
-    }
-}
+
+//     else {
+//         alert("Not Enough Money to buy orange")
+//     }
+// }
 function increaseValueThree() {
     values();
-    if (val >= 7) {
-        count_ban += 1;
-        purchase += 7;
-        val = val - purchase;
-        total_purchase = purchase + total_purchase;
-        purchase = 0;
-        document.querySelector("#number-three").value = count_ban; console.log(val);
-        result();
+    if (val > total_purchase) {
+        if (val >= 7) {
+            count_ban += 1;
+            purchase += 7;
+            val = val - purchase;
+            total_purchase = purchase + total_purchase;
+            purchase = 0;
+            document.querySelector("#number-three").value = count_ban;
+            result();
 
+        }
+        else {
+            alert("Not Enough Money to buy Banana")
+        }
     }
-    else {
+
+    else{
         alert("Not Enough Money to buy Banana")
     }
 }
